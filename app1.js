e8340044abfa2c06fea45bba3c07a195df7df5b7
bostonvictormartinez <57546685+bostonvictormartinez@users.noreply.github.com>
2021-01-21 14:52:52 -0500

    Update app1.js
    console log value(s)

--- app1.js
+++ app1.js
@@ -1,86 +1,45 @@
-/*
+var budgetController= (function(){})()
 
 var UIController= (function(){
+    
+    return{
+        getInput:function(){
+          
+            return{
+           // var type=document.querySelector('.add__type').value;
+            //var text=document.querySelector('.add__description').value;
+            //var value=document.querySelector('.add__value').value;
+        
+            type:document.querySelector('.add__type').value,
+           text:document.querySelector('.add__description').value,
+             value:document.querySelector('.add__value'.value)
+            };
+    }};   
+})()
 
-})();
 
-var budgetController= (function(){
+var controller= (function(budgetCtrl,UICtrl){
 
-})();
+    var ctrlAddItem=function () {
+        var input=UICtrl.getInput();
+        console.log(input)
+   }
 
+  //  document.querySelector('.add__btn').addEventListener('click',function (event) {
+        document.querySelector('.add__btn').addEventListener('click',ctrlAddItem)
 
-
-var controller= (function(budgetCtrl, UICtrl){
-
-        var ctrlAddItem=function(){
-            console.log('retouched.')
-    };
-
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
+        console.log('clicked')
         document.addEventListener('keypress',function (event) {
+
             console.log(event)
+
             if(event.key==='Enter'){
+                console.log('you hit enter')
                 ctrlAddItem()
-            }
-        });
-    
+            };  
+    })
 
-})(UIController, budgetController);
-
-
-*/
-
-
-
-
-
-
-var UIController= (function(){
-
-    return function () {
-        var value  =document.querySelector('.add__type').value;
-        var text=document.querySelector('.add__description').value;
-        var number=document.querySelector('.add__value').value;
-        console.log(value,text,number)
-
-        
-    }
-    
-        
-    
-
-
-
-})();
-
-var budgetController= (function(){})();
-
-var ctrlAddItem=function(){
-    console.log('clicked ctrlAddItem')
-}
-
-var controller= (function(UICtrl,budgetCtrl){
-
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
-        
-        //console.log('clicked1')
-
-        document.addEventListener('keypress',function(event){
-
-            //console.log(event)
-            if(event.key==='Enter'){
-                console.log('you hit enter');
-                console.log(UIController())
-            }
-        })
-    
-
-})(UIController,budgetController)
-
-
-
-
+})(budgetController,UIController)
 
 
 
